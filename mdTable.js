aeb73f1b39de85dc239140ce38cd4cae4e40c635
Crazy Mon <54330877+npt-dev@users.noreply.github.com>
2022-09-29 16:53:38 +0900

    Create mdTable.js

---
+++ mdTable.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+const exportTableMD = (table) => {
+  const title = []
+  const content = []
+  for (let row in table) {
+    const col = []
+    for (let key in table[row]) {
+      if (row == 0) {
+        title.push(key)
+      }
+      col.push(table[row][key])
+    }
+    content.push(col)
+  }
+
+  let mdTable = title.join(' | ')
+  mdTable += ' | \n'
+  mdTable += new Array(title.length).fill(' --- ').join(' | ')
+  mdTable += ' | \n'
+  for (let t of content) {
+    mdTable += t.join(' | ')
+    mdTable += ' | \n'
+  }
+
+  fs.writeFileSync('./mdTable.txt', mdTable)
+
+  return { title, content, mdTable }
+}
